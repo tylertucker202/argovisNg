da36b473eccfbca2d510cb3a26ce2a92bd1516b4
tylertucker202 <tylertucker202@yahoo.com>
2018-06-23 14:20:15 -1000

    using assym/ngx

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -14,27 +14,12 @@ import '../../../node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5
 export class LeafletMapComponent {
   public mProj = 'Web Mercator'
   public constructor(private mapProjectionService: MapProjectionService) {}
-
-
-  ngOnInit() {
-    console.log('im in leaflet-map.component');
-    console.log('mProj starts as:')
-    console.log(this.mProj);
-    this.mapProjectionService.change.subscribe(mProj => {
-      this.mProj = mProj;
-      console.log('im in leaflet-map.component');
-      console.log('a change occured');
-      console.log(this.mProj);
-    });
-  }
-
-  private createWebMercator() {}
-  private createSouthernStereographic() {}
-  private createNorthernStereographic() {}
-
+  layersControl = {
+    baseLayers: {},
+  };
   satelliteMap = tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    });
+  {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+  });
   googleMap = tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',
     {attribution: 'google'
   });
@@ -46,20 +31,77 @@ export class LeafletMapComponent {
     {attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
   });
 
-  layersControl = {
-    baseLayers: {
-      'Esri World Imagery ': this.satelliteMap,
-      'Google': this.googleMap, 
-      'Ocean Basemap': this.Esri_OceanBasemap,
-    },
-  };
+  ngOnInit() {
+    console.log('im in leaflet-map.component');
+    console.log('mProj starts as:')
+    console.log(this.mProj);
+    this.generateMap();
+    this.mapProjectionService.change.subscribe(mProj => {
+      this.mProj = mProj;
+      this.generateMap();
+    });
+  }
 
-  options = {
-    layers: [this.Esri_OceanBasemap],
-    zoom: 3,
-    maxBounds: [[-180, -270], [180,270]],
-    center: latLng([ 46.879966, -121.726909 ]),
-  };
+  private generateMap(this) {
+    switch(this.mProj) {
+      case 'Web Mercator': {
+        console.log('generating web mercator');
+        this.createWebMercator();
+        break;
+      }
+      case 'Southern Stereographic': {
+        console.log('generating south stereo');
+        this.createSouthernStereographic();
+        break;
+      }
+      case 'Northern Stereographic': {
+        console.log('generating north stereo');
+        this.createNorthernStereographic();
+        break;
+      }
+      default: {
+        console.log('proj not found, using web mercator')
+        this.createWebMercator();
+        break;
+      }
+    }
+  }
+
+  private createWebMercator(this) {
+    this.layersControl = {
+      baseLayers: {
+        'Esri World Imagery ': this.satelliteMap,
+        'Google': this.googleMap, 
+        'Ocean Basemap': this.Esri_OceanBasemap,
+      },
+    };
+
+    this.options = {
+      layers: [this.Esri_OceanBasemap],
+      zoom: 3,
+      maxBounds: [[-180, -270], [180,270]],
+      center: latLng([ 46.879966, -121.726909 ]),
+    };
+  }
+  
+  private createSouthernStereographic(this) {
+    this.layersControl = {
+      baseLayers: {
+        'Esri World Imagery ': this.satelliteMap,
+        'watercolor': this.watercolor, 
+      },
+    };
+
+    this.options = {
+      layers: [this.watercolor],
+      zoom: 3,
+      maxBounds: [[-180, -270], [180,270]],
+      center: latLng([ 46.879966, -121.726909 ]),
+    };
+
+  }
+
+  private createNorthernStereographic(this) {}
 
   drawnItems = featureGroup();
 
@@ -89,9 +131,37 @@ export class LeafletMapComponent {
         marker: false,
         circlemarker: false, 
         circle: false
-    }}
-  
+    }
+  }
+
     onMapReady(map: Map) {
+        /*
+        //stereographic projection
+        //var map = L.map('map', {maxZoom: 13, minZoom: 1, maxBounds: [[-180, -270], [180,270]]}).setView([0,0], 2);
+        var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                                    {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        });
+        var googleMap = L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',
+                                    {attribution: 'google'
+        });
+        var watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}',
+                                    {attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                                     subdomains: 'abcd',
+        });
+        var Esri_OceanBasemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}',
+                                    {attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+        });
+
+        L.control.layers({
+            'Esri World Imagery ': satelliteMap.addTo(map),
+            'Google': googleMap.addTo(map), 
+            'Ocean basemap': Esri_OceanBasemap.addTo(map),
+            },
+            { 'draw layer': this.drawnItems },
+            { position: 'topleft', collapsed: false }
+        ).addTo(map);
+        */
+      
       this.drawnItems.addTo(map);
       control.layers(this.layersControl.baseLayers, null, {position: 'topleft'}).addTo(map)
       L.control.coordinates({
