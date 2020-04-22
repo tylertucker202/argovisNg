8e1a0d06d44d9687ea359f3edc547b42f88df8b6
tylertucker202 <tylertucker202@gmail.com>
2020-04-22 11:58:54 -1000

    net data docker container upening. ar component removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ArComponent } from './ar/ar.component'
 import { ProfviewComponent } from './profview/profview.component'
 import { GridComponent } from './grid/grid.component';
 import { CovarComponent } from './covar/covar.component';
