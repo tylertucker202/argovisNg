40c23ec76283e75d013e3df67a90fd79ef93ff92
tylertucker202 <tylertucker202@gmail.com>
2020-07-07 11:01:16 -1000

    raster services for non uniform grid tested

--- src/app/grid/raster.service.spec.ts
+++ src/app/grid/raster.service.spec.ts
@@ -41,10 +41,35 @@ fdescribe('RasterService', () => {
     expect(mockGrid).toBeTruthy()
   }))
 
+  it('should properly index lat', inject([RasterService], (service, RasterService) => {
+    const uLats = [-5, -2.5, 0, 2.5, 5]
+
+    const t1 = 0
+    const [lat_idx_1, lat_idx_1_shift] = service.get_nearest_neighbor(uLats, t1)
+    expect(lat_idx_1).toEqual(2)
+    expect(lat_idx_1_shift).toEqual(1)
+    const t2 = -0.1
+    const [lat_idx_2, lat_idx_2_shift] = service.get_nearest_neighbor(uLats, t2)
+    expect(lat_idx_2).toEqual(2)
+    expect(lat_idx_2_shift).toEqual(-1)
+    const t3 = -2.4
+    const [lat_idx_3, lat_idx_3_shift] = service.get_nearest_neighbor(uLats, t3)
+    expect(lat_idx_3).toEqual(1)
+    expect(lat_idx_3_shift).toEqual(1)
+    const t4 = 2.6
+    const [lat_idx_4, lat_idx_4_shift] = service.get_nearest_neighbor(uLats, t4)
+    expect(lat_idx_4).toEqual(3)
+    expect(lat_idx_4_shift).toEqual(1)
+    const t5 = 4.6
+    const [lat_idx_5, lat_idx_5_shift] = service.get_nearest_neighbor(uLats, t5)
+    expect(lat_idx_5).toEqual(4)
+    expect(lat_idx_5_shift).toEqual(-1)
+  }))
+
   it('should make grid arrays', inject([RasterService], (service: RasterService) => {
     service.getMockGrid()
     .subscribe( grid => {
-      const [uLats, nLats, uLons, nLons, valuesMatrix] = service.make_grid_arrays(grid[0].data);
+      const [uLats, uLons, valuesMatrix] = service.make_grid_arrays(grid[0].data);
       expect(uLats).toBeTruthy()
       expect(uLons).toBeTruthy()
       for (let idx = 1; idx<uLats.length; ++idx) {
@@ -54,19 +79,19 @@ fdescribe('RasterService', () => {
         expect(uLons[idx]).toBeGreaterThan(uLons[idx-1])
       }
       // console.log('nLats', nLats, 'nLons', nLons)
-      expect(nLats).toEqual(39)
-      expect(nLons).toEqual(15)
+      expect(uLats.length).toEqual(39)
+      expect(uLons.length).toEqual(15)
       // console.log('valuesMatrix', valuesMatrix.length, valuesMatrix[0].length)
-      expect(valuesMatrix.length).toEqual(nLats)
-      expect(valuesMatrix[0].length).toEqual(nLons)
+      expect(valuesMatrix.length).toEqual(uLats.length)
+      expect(valuesMatrix[0].length).toEqual(uLons.length)
       const lat = -67.5
       const lon = 1.25
       const dlon = uLons[1] - uLons[0]
       const minLon = uLons[0]
-      const lon_idx = service.get_lon_idx(lon, dlon, minLon)
+      const lon_idx = service.get_uniform_idx(lon, dlon, minLon)
       expect(lon_idx===4)
       expect(uLons[lon_idx] === 1.5)
-      const [lat_idx, lat_shift] = service.get_lat_idx(uLats, lat)
+      const [lat_idx, lat_shift] = service.get_nearest_neighbor(uLats, lat)
       expect(lat_shift===-1)
       expect(lat_idx===20)
       expect(uLats[lat_idx] === -67.519)
@@ -79,30 +104,115 @@ fdescribe('RasterService', () => {
       const intpValue = service.bilinear_interpolation(lon, lat, points)
       console.log('points', points, 'intpValue', intpValue)
       expect(intpValue).toEqual(0.00007948320375250093)
+    })
+  }))
 
+  it('should make regrid', inject([RasterService], (service: RasterService) => {
+    service.getMockGrid()
+    .subscribe( grid => {
+      const [uLats, uLons, valuesMatrix] = service.make_grid_arrays(grid[0].data);
+      const delta = .25
+      const star_lats = service.makeRegridArray(uLats, delta)
+      const star_lons = service.makeRegridArray(uLons, delta)
+      // console.log('uLats', uLats, 'star_lats', star_lats)
+      // console.log('uLons', uLons, 'star_lons', star_lons)
+
+      expect(star_lats[0]).toBeGreaterThanOrEqual(uLats[0], 'extrapolating not allowed')
+      expect(star_lats[star_lats.length-1]).toBeLessThanOrEqual(uLats[uLats.length-1], 'extrapolating not allowed')
+      for (let idx=1; idx<star_lats.length; ++idx) {
+        const delta_star = star_lats[idx] - star_lats[idx-1]
+        expect(delta_star).toBeGreaterThan(0, 'always be increasing')
+        expect(delta_star.toFixed(5)).toEqual(delta.toFixed(5), 'delta has changed')
+      }
+
+      expect(star_lons[0]).toBeGreaterThanOrEqual(uLons[0], 'extrapolating not allowed')
+      expect(star_lons[star_lons.length-1]).toBeLessThanOrEqual(uLons[uLons.length-1], 'extrapolating not allowed')
+      for (let idx=1; idx<star_lons.length; ++idx) {
+        const delta_star = star_lons[idx] - star_lons[idx-1]
+        expect(delta_star).toBeGreaterThan(0, 'always be increasing')
+        expect(delta_star.toFixed(5)).toEqual(delta.toFixed(5), 'delta has changed')
+      }
+    })
+  }))
+
+  
+  it('should do bilinear interpolation', inject([RasterService], (service: RasterService) => {
+    service.getMockGrid()
+    .subscribe( grid => {
+      const delta = .25
+      const [uLats, uLons, valuesMatrix] = service.make_grid_arrays(grid[0]['data']);
+      const star_lats = service.makeRegridArray(uLats, delta)
+      const star_lons = service.makeRegridArray(uLons, delta)
+      let points = []
+      for (let idx=0; idx<star_lats.length; ++idx) {
+        for (let jdx=0; jdx<star_lons.length; ++jdx) {
+          points.push([star_lats[idx], star_lons[jdx]])
+        }
+      }
+      let zs = []
+      const dlon = uLons[1] - uLons[0]
+      const minLon = Math.min(...uLons)
+      // console.log('ulats: ', uLats)
+      points.forEach( ([lat, lon]) => {
+        const [lat_idx, lat_shift] = service.get_nearest_neighbor(uLats, lat)
+        const lon_idx = service.get_uniform_idx(lon, dlon, minLon)
+        // console.log('lat, lon, lon_idx, lat_idx, latshift:', lat, lon, lon_idx, lat_idx, lat_shift)
+        const llPoint = [uLons[lon_idx], uLats[lat_idx], valuesMatrix[lat_idx][lon_idx]] as [number, number, number]
+        const lrPoint = [uLons[lon_idx+1], uLats[lat_idx], valuesMatrix[lat_idx][lon_idx+1]] as [number, number, number]
+        const urPoint = [uLons[lon_idx+1], uLats[lat_idx+lat_shift], valuesMatrix[lat_idx+lat_shift][lon_idx+1]] as [number, number, number]
+        const ulPoint = [uLons[lon_idx], uLats[lat_idx+lat_shift], valuesMatrix[lat_idx+lat_shift][lon_idx]] as [number, number, number]
+        let points = [llPoint, lrPoint, urPoint, ulPoint]
+        // console.log('lat lng intp', lat, lon, 'points', points)
+        points = points.sort(function(a, b) {
+          if (a[0] == b[0]) {
+            return a[1] - b[1];
+          }
+          return a[0] - b[0];
+        })
+        const [x1, y1, q11] = points[0]
+        const [_x1, y2, q12] = points[1] 
+        const [x2, _y1, q21] = points[2]
+        const [_x2, _y2, q22] = points[3]
+        if ( x1 !== _x1 || x2 !== _x2 || y1 !== _y1 || y2 !== _y2) {
+          expect(false).toEqual(true, 'points do not form a rectangle')
+        }
+        if ( lon < x1 || lon > x2 || lat < y1 || lat > y2) {
+          expect(false).toEqual(true, '(x,y) do not lie within rectangle')
+        }
+        const intpValue = service.bilinear_interpolation(lon, lat, points)
+        zs.push(intpValue)
+      })
+    })
+  }))
+
+  it('should make rasterGrid from grid', inject([RasterService], (service: RasterService) => {
+    service.getMockGrid()
+    .subscribe( grid => {
+      const delta = .25
+      // console.log('grid', grid[0]['data'][0:10])
+      expect(delta).toBeTruthy()
+      const raster = service.makeRasterFromGrid(grid[0], delta)
+      expect(raster).toBeTruthy()
+      // console.log('raster', raster)
     })
   }))
   
-  // it('should get lat and lon idx ', inject([RasterService], (service: RasterService) => {
-  //   const lat = -67.5
-  //   const lon = 1.25
-  //   service.getMockGrid()
-  //   .subscribe( grid => {
-  //     const [uLats, nLats, uLons, nLons, valuesMatrix] = service.make_grid_arrays(grid[0].data);
-  //     const dlon = uLons[1] - uLons[0]
-  //     const minLon = Math.min(uLons)
-  //     const lat_idx = this.get_lat_idx(uLats, lat)
-  //     expect(lat_idx===20)
-  //     expect(uLats[lat_idx] === -67.519)
-  //     const lon_idx = this.get_lon_idx(lon, dlon, minLon)
-  //     expect(lon_idx===4)
-  //     expect(uLons[lon_idx] === 1.5)
-  //   })
-  // }))
-
-  // it('should perform bilinear interpolation', inject([RasterService], (service: RasterService) => {
-  //   const mockGrid = service.getMockGrid()
-  // }))
+  it('should get lat and lon idx ', inject([RasterService], (service: RasterService) => {
+    const lat = -67.5
+    const lon = 1.25
+    service.getMockGrid()
+    .subscribe( grid => {
+      const [uLats, uLons, valuesMatrix] = service.make_grid_arrays(grid[0]['data']);
+      const dlon = uLons[1] - uLons[0]
+      const minLon = Math.min(...uLons)
+      const [lat_idx, lat_shift] = service.get_nearest_neighbor(uLats, lat)
+      expect(lat_idx).toEqual(20)
+      expect(uLats[lat_idx]).toEqual(-67.519)
+      const lon_idx = service.get_uniform_idx(lon, dlon, minLon)
+      expect(lon_idx).toEqual(3)
+      expect(uLons[lon_idx]).toEqual(1.167)
+    })
+  }))
 
   // it('should make canvas layer', inject([RasterService], (service: RasterService) => {
   //   const mockGrid = service.getMockGridRaster()
@@ -114,9 +224,8 @@ fdescribe('RasterService', () => {
   //     const interpolateBool = false;
   //     const map = L.map('#map')
   //     const makeCanvasLayer = service['makeCanvasLayer']
-  //     const layer = makeCanvasLayer(grid, brewerColorScheme, interpolateBool, map)
+  //     const layer = makeCanvasLayer(grid, brewerColorScheme,true, interpolateBool, map)
   //     expect(layer).toBeTruthy()
-  //     expect(layer.options.interpolate).toEqual(false)
   //   },
   //     error => {
   //       console.log('error in getting mock profiles' )
--- src/app/grid/raster.service.ts
+++ src/app/grid/raster.service.ts
@@ -44,7 +44,7 @@ export class RasterService {
     return lon
   }
 
-  public make_grid_arrays(gridCells: GridCell[]) {
+  public make_grid_arrays(gridCells: GridCell[]): [number[], number[], number[][]] {
     //assumes lat and lon are sorted
     let lats = []
     let lons = []
@@ -72,59 +72,100 @@ export class RasterService {
 
     let matrix = [];
     while(values.length) { matrix.push(values.splice(0,nLons)) };
-    return [uLats, nLats, uLons, nLons, matrix]
+    return [uLats, uLons, matrix]
+  }
+
+  public makeRegridArray(arr: number[], delta: number): number[] {
+    //find n columns of star_arr
+    const rangeArr = arr[arr.length -1] - arr[0]
+    const ncols = Math.floor(rangeArr / delta)
+    //set first star_arr element
+    const rem = rangeArr % delta
+    let star_arr = [ arr[0] + rem/2 ]
+    // populate star_arr
+    while (star_arr.length < ncols ) {
+      star_arr.push(star_arr[star_arr.length-1] + delta)
+    }
+    return star_arr
+  }
+
+  public makeRasterFromGrid(grid: Grid, delta: number): RasterGrid {
+    //assumes lat lng points are inside grid area
+    let raster = {}
+    raster['_id'] = grid['_id']
+    raster['gridName'] = grid['gridName']
+    raster['measurement'] = grid['measurement']
+    raster['pres'] = grid['pres']
+    raster['date'] = grid['date']
+    raster['units'] = grid['units']
+    const [uLats, uLons, valuesMatrix] = this.make_grid_arrays(grid['data']);
+    const star_lats = this.makeRegridArray(uLats, delta)
+    const star_lons = this.makeRegridArray(uLons, delta)
+    raster['cellXSize'] = delta
+    raster['cellYSize'] = delta
+    raster['xllCorner'] = star_lons[0]
+    raster['yllCorner'] = star_lats[0]
+
+    let points = []
+    for (let idx=0; idx<star_lats.length; ++idx) {
+      for (let jdx=0; jdx<star_lons.length; ++jdx) {
+        points.push([star_lats[idx], star_lons[jdx]])
+      }
+    }
+    raster['zs'] = this.interpolateGrid(points, valuesMatrix, uLats, uLons)
+
+    return raster as RasterGrid
   }
 
 
-  public makeRasterFromGrid(grid: Grid, latLngPoints: [number, number][], lon_0: number): RasterGrid {
-    //assumes lat lng points are inside grid area
-    let raster: RasterGrid
-    raster._id = grid._id
-    raster.gridName = grid.gridName
-    raster.measurement = grid.measurement
-    raster.pres = grid.pres
-    raster.date = grid.date
-    raster.units = grid.units
-
-    //reshape grid data
-    const [uLats, nLats, uLons, nLons, valuesMatrix] = this.make_grid_arrays(grid.data);
+  public interpolateGrid(latLngPoints: [number, number][], valuesMatrix: number[][],  uLats: number[], uLons: number[] ): number[] {
 
     //longitude is a uniform grid
     const dlon = uLons[1] - uLons[0]
-    const minLon = Math.min(uLons)
+    const minLon = Math.min(...uLons)
 
     let zs = []
     latLngPoints.forEach( ([lat, lon]) => {
-      const [lat_idx, lat_shift] = this.get_lat_idx(uLats.tolist(), lat)
-      const lon_idx = this.get_lon_idx(lon, dlon, minLon)
-      const llPoint = [uLons[lon_idx], uLats[lat_idx], valuesMatrix[lon_idx][lat_idx]] as [number, number, number]
-      const lrPoint = [uLons[lon_idx+1], uLats[lat_idx], valuesMatrix[lon_idx+1][lat_idx]] as [number, number, number]
-      const urPoint = [uLons[lon_idx+1], uLats[lat_idx+lat_shift], valuesMatrix[lon_idx+1][lat_idx+lat_shift]] as [number, number, number]
-      const ulPoint = [uLons[lon_idx], uLats[lat_idx+lat_shift], valuesMatrix[lon_idx][lat_idx+lat_shift]] as [number, number, number]
+      const [lat_idx, lat_shift] = this.get_nearest_neighbor(uLats, lat)
+      const lon_idx = this.get_uniform_idx(lon, dlon, minLon)
+      const llPoint = [uLons[lon_idx], uLats[lat_idx], valuesMatrix[lat_idx][lon_idx]] as [number, number, number]
+      const lrPoint = [uLons[lon_idx+1], uLats[lat_idx], valuesMatrix[lat_idx][lon_idx+1]] as [number, number, number]
+      const urPoint = [uLons[lon_idx+1], uLats[lat_idx+lat_shift], valuesMatrix[lat_idx+lat_shift][lon_idx+1]] as [number, number, number]
+      const ulPoint = [uLons[lon_idx], uLats[lat_idx+lat_shift], valuesMatrix[lat_idx+lat_shift][lon_idx]] as [number, number, number]
       const points = [llPoint, lrPoint, urPoint, ulPoint]
       const intpValue = this.bilinear_interpolation(lon, lat, points)
-      zs.push()
+      zs.push(intpValue)
     })
-    raster.zs = zs
-    return raster
+    return zs
   }
 
-  public get_lon_idx(lon: number, dlon: number, lon_0: number) {
+  public get_uniform_idx(lon: number, dlon: number, lon_0: number) {
     // finds idx of a uniformly spaced grid
     return  Math.floor((lon - lon_0) / dlon)
   }
-  public get_lat_idx(arr, target): [number, number] {
+  public get_nearest_neighbor(arr, target): [number, number] {
     //binary search of nearest neighbor for an array of numbers
+    // todo: come up with a starting guess to reduce the number of steps?.
+
     //corner cases
-    if (target <= arr[0]) { return [0, 1] }
-    if (target >= arr[ arr.length -1 ]) { return [arr.length - 1, -1]}
+    if (target <= arr[0]) { 
+      // console.log('beginning corner case')
+      return [0, 1] 
+    }
+    if (target >= arr[ arr.length -1 ]) { 
+      // console.log('ending corner case')
+      return [arr.length - 1, -1]
+    }
 
     //iterative binary search
-    let idx = 0; let jdx = arr.length; let mid = 0;
+    let idx = 0; let jdx = arr.length-1; let mid = 0;
+
+    // console.log('target: ', target, 'arr[idx]', arr[idx], 'arr[jdx]', arr[jdx])
     while (idx < jdx) {
       mid = Math.ceil( (idx + jdx) / 2)
 
       if (arr[mid] === target) {
+        // console.log('target is midpoint', mid, jdx, idx)
         return [mid, 1]
       }
 
@@ -132,26 +173,32 @@ export class RasterService {
         // if target is greater than previous 
         // to mid, return closest of two
         if (mid > 0 && target > arr[mid-1]) {
-          const closest_idx = this.get_closest_idx(arr[mid - 1], arr[mid], mid, target)
-          return [closest_idx, -1]
+          const [closest_idx, shift] = this.get_closest_idx(arr[mid - 1], arr[mid], mid-1, target)
+          // console.log('idx is to the left of target')
+          return [closest_idx, shift]
         }
         jdx = mid
+        // console.log('target: ', target, 'arr[idx]', arr[idx], 'arr[jdx]', arr[jdx])
       }
       else { // search to the right of mid
         if (mid < arr.length - 1 && target < arr[mid + 1]) {
-          const closest_idx = this.get_closest_idx(arr[mid-1], arr[mid], mid, target)
-          return [closest_idx, 1]
+          // console.log('idx is to the right of target')
+          const [ closest_idx, shift ] = this.get_closest_idx(arr[mid], arr[mid+1], mid, target)
+          return [closest_idx, shift]
         }
         idx = mid + 1
+        // console.log('target: ', target, 'arr[idx]', arr[idx], 'arr[jdx]', arr[jdx])
       }
-    // Only single element after search
-    return [arr[mid], 1]
     }
+    // Only single element after search
+    // console.log('only single element returned after search')
+    return [mid, 1]
+    
   }
 
-  public get_closest_idx(val1: number, val2: number, mid: number, target: number): number {
-      if (target - val1 >= val2 - target) { return mid }
-      else { return mid-1}
+  public get_closest_idx(val1: number, val2: number, val1_idx: number, target: number): [number, number] {
+      if (target - val1 >= val2 - target) { return [val1_idx+1, -1] }
+      else { return [val1_idx, 1]}
   }
 
   public bilinear_interpolation(x: number, y: number, points: [number, number, number][]) {
@@ -167,7 +214,12 @@ export class RasterService {
         165.0
     See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
     */
-    points = points.sort()
+    points = points.sort(function(a, b) {
+      if (a[0] == b[0]) {
+        return a[1] - b[1];
+      }
+      return a[0] - b[0];
+    })
     const [x1, y1, q11] = points[0]
     const [_x1, y2, q12] = points[1] 
     const [x2, _y1, q21] = points[2]
@@ -176,7 +228,7 @@ export class RasterService {
       console.error('points do not form a rectangle')
     }
     if ( x < x1 || x > x2 || y < y1 || y > y2) {
-      console.error('(x,y) do not lie within rectangle')
+      console.error('(x,y) do not lie within rectangle', x, y, 'points:', points)
     }
     return (q11 * (x2 - x) * (y2 - y) +
             q21 * (x - x1) * (y2 - y) +
