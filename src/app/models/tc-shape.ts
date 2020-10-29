2dca06572204946d668cf9abfaac17249e3ddd06
tylertucker202 <tylertucker202@gmail.com>
2020-10-29 09:25:47 -1000

    mock shapes

--- src/app/models/tc-shape.ts
+++ src/app/models/tc-shape.ts
@@ -1,6 +1,40 @@
+import { PlatformMeta } from './../profview/profiles.d';
 export interface TcShape {
+	_id : string,
+	name : string,
+	num : number,
+	source : string,
+	traj_data : TrajData[]
+}
+
+export interface TrajData {
+    time: number
+    date: string
+    l?: string
+    class?: string
+    lat: number
+    lon: number
+    wind?: number
+    pres?: number
+    year?: number
+    timestamp: Date
+    geoLocation: {type: string, coordinates: Array<number> };
+}
+
+export interface TcTrajShape {
     _id: string
-    shapeId: number
-    geoLocation: {type: string, coordinates: Array<Array<number>> };
-    date: Date
+    time: number
+    name: string
+    num: number
+    date: string
+    l?: string
+    class?: string
+    lat: number
+    lon: number
+    wind?: number
+    pres?: number
+    year?: number
+    timestamp: Date
+    source: string
+    geoLocation: {type: string, coordinates: Array<number> };
 }
--- src/app/tc/tc-shape.parameters.ts
+++ src/app/tc/tc-shape.parameters.ts
@@ -1,11 +1,374 @@
-import { TcShape } from '../models/tc-shape'
+import { TcShape, TcTrajShape } from '../models/tc-shape'
 
-export const mockTcShape: TcShape = {
-    _id: "1_262992.0",
-    shapeId: 1,
-    geoLocation: {
-      "type" : "Polygon",
-      "coordinates" : [[-5, -5],[-5, 5],[5, 5],[5, -5],[-5,-5]]
-    },
-    date: new Date("2010-01-01T00:00:00Z")
-  }
+export const carlottaTraj : TcTrajShape[] = [
+{ _id : "EP042018_2018-06-14_18:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-14", time : 1800, class : "TD", lat : 14.9, lon : 100, wind : 30, pres : 1008, year : 2018, timestamp : new Date("2018-06-14T18:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 100, 14.9 ] } },
+{ _id : "EP042018_2018-06-15_00:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-15", time : 0, class : "TD", lat : 15.1, lon : 100, wind : 30, pres : 1007, year : 2018, timestamp : new Date("2018-06-15T00:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 100, 15.1 ] } },,
+{ _id : "EP042018_2018-06-15_06:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-15", time : 600, class : "TD", lat : 15.4, lon : 99.9, wind : 30, pres : 1007, year : 2018, timestamp : new Date("2018-06-15T06:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.9, 15.4 ] } },
+{ _id : "EP042018_2018-06-15_12:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-15", time : 1200, class : "TD", lat : 15.6, lon : 99.8, wind : 30, pres : 1007, year : 2018, timestamp : new Date("2018-06-15T12:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.8, 15.6 ] } },
+{ _id : "EP042018_2018-06-15_18:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-15", time : 1800, class : "TS", lat : 15.8, lon : 99.6, wind : 35, pres : 1006, year : 2018, timestamp : new Date("2018-06-15T18:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.6, 15.8 ] } },
+{ _id : "EP042018_2018-06-16_00:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-16", time : 0, class : "TS", lat : 15.9, lon : 99.4, wind : 35, pres : 1006, year : 2018, timestamp : new Date("2018-06-16T00:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.4, 15.9 ] } },
+{ _id : "EP042018_2018-06-16_06:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-16", time : 600, class : "TS", lat : 15.9, lon : 99.3, wind : 45, pres : 1000, year : 2018, timestamp : new Date("2018-06-16T06:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.3, 15.9 ] } },
+{ _id : "EP042018_2018-06-16_12:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-16", time : 1200, class : "TS", lat : 15.9, lon : 99.2, wind : 45, pres : 1000, year : 2018, timestamp : new Date("2018-06-16T12:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.2, 15.9 ] } },
+{ _id : "EP042018_2018-06-16_18:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-16", time : 1800, class : "TS", lat : 16, lon : 99.2, wind : 45, pres : 1000, year : 2018, timestamp : new Date("2018-06-16T18:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.2, 16 ] } },
+{ _id : "EP042018_2018-06-17_00:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-17", time : 0, class : "TS", lat : 16.4, lon : 99.5, wind : 55, pres : 997, year : 2018, timestamp : new Date("2018-06-17T00:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 99.5, 16.4 ] } },
+{ _id : "EP042018_2018-06-17_06:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-17", time : 600, class : "TS", lat : 16.6, lon : 100.4, wind : 45, pres : 1000, year : 2018, timestamp : new Date("2018-06-17T06:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 100.4, 16.6 ] } },
+{ _id : "EP042018_2018-06-17_12:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-17", time : 1200, class : "TS", lat : 16.8, lon : 101, wind : 35, pres : 1005, year : 2018, timestamp : new Date("2018-06-17T12:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 101, 16.8 ] } },
+{ _id : "EP042018_2018-06-17_18:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-17", time : 1800, class : "TD", lat : 17, lon : 101.7, wind : 25, pres : 1007, year : 2018, timestamp : new Date("2018-06-17T18:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 101.7, 17 ] } },
+{ _id : "EP042018_2018-06-18_00:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-18", time : 0, class : "TD", lat : 17.2, lon : 102.2, wind : 25, pres : 1007, year : 2018, timestamp : new Date("2018-06-18T00:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 102.2, 17.2 ] } },
+{ _id : "EP042018_2018-06-18_06:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-18", time : 600, class : "TD", lat : 17.4, lon : 102.6, wind : 25, pres : 1008, year : 2018, timestamp : new Date("2018-06-18T06:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 102.6, 17.4 ] } },
+{ _id : "EP042018_2018-06-18_12:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-18", time : 1200, class : "TD", lat : 17.7, lon : 103, wind : 25, pres : 1008, year : 2018, timestamp : new Date("2018-06-18T12:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 103, 17.7 ] } },
+{ _id : "EP042018_2018-06-18_18:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-18", time : 1800, class : "TD", lat : 18, lon : 103.3, wind : 25, pres : 1007, year : 2018, timestamp : new Date("2018-06-18T18:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 103.3, 18 ] } },
+{ _id : "EP042018_2018-06-19_00:00:00_HURDAT2", name : "CARLOTTA", num : 18, date : "2018-06-19", time : 0, class : "LO", lat : 18.1, lon : 103.5, wind : 20, pres : 1008, year : 2018, timestamp : new Date("2018-06-19T00:00:00Z"), source : "HURDAT2", geoLocation : { type : "point", coordinates : [ 103.5, 18.1 ] } },
+]
+
+export const carlotta: TcShape = 
+{
+	_id : "EP042018_HURDAT2",
+	name : "CARLOTTA",
+	num : 18,
+	source : "HURDAT2",
+	traj_data : [
+		{
+			date : "2018-06-14",
+			time : 1800,
+			l : "",
+			class : "TD",
+			lat : 14.9,
+			lon : 100,
+			wind : 30,
+			pres : 1008,
+			year : 2018,
+			timestamp : new Date("2018-06-14T18:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					100,
+					14.9
+				]
+			}
+		},
+		{
+			date : "2018-06-15",
+			time : 0,
+			l : "",
+			class : "TD",
+			lat : 15.1,
+			lon : 100,
+			wind : 30,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-15T00:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					100,
+					15.1
+				]
+			}
+		},
+		{
+			date : "2018-06-15",
+			time : 600,
+			l : "",
+			class : "TD",
+			lat : 15.4,
+			lon : 99.9,
+			wind : 30,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-15T06:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.9,
+					15.4
+				]
+			}
+		},
+		{
+			date : "2018-06-15",
+			time : 1200,
+			l : "",
+			class : "TD",
+			lat : 15.6,
+			lon : 99.8,
+			wind : 30,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-15T12:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.8,
+					15.6
+				]
+			}
+		},
+		{
+			date : "2018-06-15",
+			time : 1800,
+			l : "",
+			class : "TS",
+			lat : 15.8,
+			lon : 99.6,
+			wind : 35,
+			pres : 1006,
+			year : 2018,
+			timestamp : new Date("2018-06-15T18:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.6,
+					15.8
+				]
+			}
+		},
+		{
+			date : "2018-06-16",
+			time : 0,
+			l : "",
+			class : "TS",
+			lat : 15.9,
+			lon : 99.4,
+			wind : 35,
+			pres : 1006,
+			year : 2018,
+			timestamp : new Date("2018-06-16T00:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.4,
+					15.9
+				]
+			}
+		},
+		{
+			date : "2018-06-16",
+			time : 600,
+			l : "",
+			class : "TS",
+			lat : 15.9,
+			lon : 99.3,
+			wind : 45,
+			pres : 1000,
+			year : 2018,
+			timestamp : new Date("2018-06-16T06:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.3,
+					15.9
+				]
+			}
+		},
+		{
+			date : "2018-06-16",
+			time : 1200,
+			l : "",
+			class : "TS",
+			lat : 15.9,
+			lon : 99.2,
+			wind : 45,
+			pres : 1000,
+			year : 2018,
+			timestamp : new Date("2018-06-16T12:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.2,
+					15.9
+				]
+			}
+		},
+		{
+			date : "2018-06-16",
+			time : 1800,
+			l : "",
+			class : "TS",
+			lat : 16,
+			lon : 99.2,
+			wind : 45,
+			pres : 1000,
+			year : 2018,
+			timestamp : new Date("2018-06-16T18:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.2,
+					16
+				]
+			}
+		},
+		{
+			date : "2018-06-17",
+			time : 0,
+			l : "",
+			class : "TS",
+			lat : 16.4,
+			lon : 99.5,
+			wind : 55,
+			pres : 997,
+			year : 2018,
+			timestamp : new Date("2018-06-17T00:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					99.5,
+					16.4
+				]
+			}
+		},
+		{
+			date : "2018-06-17",
+			time : 600,
+			l : "",
+			class : "TS",
+			lat : 16.6,
+			lon : 100.4,
+			wind : 45,
+			pres : 1000,
+			year : 2018,
+			timestamp : new Date("2018-06-17T06:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					100.4,
+					16.6
+				]
+			}
+		},
+		{
+			date : "2018-06-17",
+			time : 1200,
+			l : "",
+			class : "TS",
+			lat : 16.8,
+			lon : 101,
+			wind : 35,
+			pres : 1005,
+			year : 2018,
+			timestamp : new Date("2018-06-17T12:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					101,
+					16.8
+				]
+			}
+		},
+		{
+			date : "2018-06-17",
+			time : 1800,
+			l : "",
+			class : "TD",
+			lat : 17,
+			lon : 101.7,
+			wind : 25,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-17T18:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					101.7,
+					17
+				]
+			}
+		},
+		{
+			date : "2018-06-18",
+			time : 0,
+			l : "",
+			class : "TD",
+			lat : 17.2,
+			lon : 102.2,
+			wind : 25,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-18T00:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					102.2,
+					17.2
+				]
+			}
+		},
+		{
+			date : "2018-06-18",
+			time : 600,
+			l : "",
+			class : "TD",
+			lat : 17.4,
+			lon : 102.6,
+			wind : 25,
+			pres : 1008,
+			year : 2018,
+			timestamp : new Date("2018-06-18T06:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					102.6,
+					17.4
+				]
+			}
+		},
+		{
+			date : "2018-06-18",
+			time : 1200,
+			l : "",
+			class : "TD",
+			lat : 17.7,
+			lon : 103,
+			wind : 25,
+			pres : 1008,
+			year : 2018,
+			timestamp : new Date("2018-06-18T12:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					103,
+					17.7
+				]
+			}
+		},
+		{
+			date : "2018-06-18",
+			time : 1800,
+			l : "",
+			class : "TD",
+			lat : 18,
+			lon : 103.3,
+			wind : 25,
+			pres : 1007,
+			year : 2018,
+			timestamp : new Date("2018-06-18T18:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					103.3,
+					18
+				]
+			}
+		},
+		{
+			date : "2018-06-19",
+			time : 0,
+			l : "",
+			class : lO",
+			lat : 18.1,
+			lon : 103.5,
+			wind : 20,
+			pres : 1008,
+			year : 2018,
+			timestamp : new Date("2018-06-19T00:00:00Z"),
+			geoLocation : {
+				type : "point",
+				coordinates : [
+					103.5,
+					18.1
+				]
+			}
+		}
+	]
+}
