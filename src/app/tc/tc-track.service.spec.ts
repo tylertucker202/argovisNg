3fdc8032ece7edec9d20d6028d7b08d61190cc28
tylertucker202 <tylertucker202@gmail.com>
2020-11-25 17:21:20 -1000

    fixed test

--- src/app/tc/tc-track.service.spec.ts
+++ src/app/tc/tc-track.service.spec.ts
@@ -1,20 +1,21 @@
+import { TcQueryService } from './tc-query.service';
 import { PopupCompileService } from './../home/services/popup-compile.service';
 import { TcMapService } from './tc-map.service';
 import { MapService } from './../home/services/map.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TcTrackService } from './tc-track.service';
-
 describe('TcTrackService', () => {
   let service: TcTrackService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
       providers: [
         MapService,
         TcMapService,
+        TcQueryService,
         PopupCompileService,
       ]
     });
