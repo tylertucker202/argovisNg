b6a15aa0114066c623840e08b8c1022b9ec5a0dc
tylertucker202 <tylertucker202@gmail.com>
2021-02-09 20:26:01 -1000

    npm audit fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { GridModule } from './grid/grid.module';
